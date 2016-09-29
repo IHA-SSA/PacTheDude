86443b518a248a4deb8fa2b15cd69eb96265c145
XoLMeS <Vika@MacBook-Air-2.mshome.net>
2016-09-29 15:19:08 +0300

    conf

--- src/xolmes/World.java
+++ src/xolmes/World.java
@@ -16,6 +16,7 @@ import acm.program.GraphicsProgram;
 
 public class World extends GraphicsProgram {
 
+<<<<<<< Updated upstream
     public static final int BLOCK_SIZE = 60;
     private static final Color BACKGROUND_COLOR = Color.WHITE;
     WorldCreator wc;
@@ -246,4 +247,171 @@ public class World extends GraphicsProgram {
         this.remove(diamond);
     }
 
+=======
+	public static final int BLOCK_SIZE = 60;
+	private static final Color BACKGROUND_COLOR = Color.WHITE;
+	WorldCreator wc;
+	Graph labirinth;
+	PacTheDude pac;
+	GPolygon diamond;
+	int aim_x = 0;
+	int aim_y = 5;
+	public void run() {
+
+		wc = new WorldCreator();
+		labirinth = new Graph("labirinth_test.txt");
+		pac = new PacTheDude(0, 9);
+		drawWorld();
+		add(pac.getPac());
+		add(pac.getPath());
+		
+		//ADD AIM 
+		diamond = new GPolygon();
+		diamond.addVertex(BLOCK_SIZE/6, 0);
+		diamond.addVertex(BLOCK_SIZE/3, BLOCK_SIZE/6);
+		diamond.addVertex(BLOCK_SIZE/6, BLOCK_SIZE/3);
+		diamond.addVertex(0, BLOCK_SIZE/6);
+		diamond.setFilled(true);
+		diamond.setFillColor(Color.GRAY);
+		diamond.sendBackward();
+		add(diamond,aim_x*BLOCK_SIZE + BLOCK_SIZE/3, aim_y*BLOCK_SIZE + BLOCK_SIZE/3);
+		
+		this.getGCanvas().addKeyListener(new KeyListener() {
+
+			@Override
+			public void keyTyped(KeyEvent e) {
+
+			}
+
+			@Override
+			public void keyReleased(KeyEvent e) {
+
+				if (e.getKeyChar() == 'w') {
+					if (pac.getY() > 0) {
+						pac.move(pac.getX(), pac.getY() - 1);
+					}
+				}
+				if (e.getKeyChar() == 'a') {
+					if (pac.getX() > 0) {
+						pac.move(pac.getX() - 1, pac.getY());
+					}
+				}
+				if (e.getKeyChar() == 's') {
+					if (pac.getY() < labirinth.Y() - 1) {
+						pac.move(pac.getX(), pac.getY() + 1);
+					}
+				}
+				if (e.getKeyChar() == 'd') {
+					if (pac.getX() < labirinth.X() - 1) {
+						pac.move(pac.getX() + 1, pac.getY());
+					}
+				}
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void keyPressed(KeyEvent e) {
+				// TODO Auto-generated method stub
+
+			}
+		});
+		//BreadthFirstPaths bfs = new BreadthFirstPaths(labirinth,pac.getX()+pac.getY()*labirinth.X());
+		//followThePath(bfs.pathTo(aim_x+aim_y*labirinth.Y()));
+	
+		//randomMakesMeCrazy();
+	}
+
+	public void drawWorld() {
+		this.resize(labirinth.X() * BLOCK_SIZE, labirinth.Y() * BLOCK_SIZE);
+		this.setBackground(BACKGROUND_COLOR);
+
+		ArrayList<Integer> sides = new ArrayList<Integer>();
+		for (int i = 0; i < labirinth.X() * labirinth.Y(); i++) {
+
+			boolean top = false;
+			boolean right = false;
+			boolean bottom = false;
+			boolean left = false;
+			sides.add(i + 1);
+			sides.add(i - 1);
+			sides.add(i + labirinth.X());
+			sides.add(i - labirinth.X());
+			for (Integer in : labirinth.adj(i)) {
+
+				if (sides.contains(in)) {
+					if (in == i + 1) {
+						right = true;
+					}
+					if (in == i - 1) {
+						left = true;
+					}
+					if (in == i + labirinth.X()) {
+						bottom = true;
+					}
+					if (in == i - labirinth.X()) {
+						top = true;
+					}
+				}
+
+			}
+			sides.clear();
+			if (!top && !bottom && !left && !right) {
+				//add(wc.createBlock(i % labirinth.X(), i / labirinth.X(), top, right, bottom, left, true));
+			} else {
+				add(wc.createBlock(i % labirinth.X(), i / labirinth.X(), top, right, bottom, left, false));
+			}
+		}
+	}
+	
+	private void randomMakesMeCrazy(){
+		Random r = new Random();
+		while (true) {
+			int x = r.nextInt(3)-1;
+			int y = r.nextInt(3)-1;
+			
+			if (pac.getY()+y >= 0 && pac.getX()+x>=0 && pac.getY()+y<=labirinth.Y()-1 && pac.getX()+x<=labirinth.X()-1) {
+				pac.move(pac.getX()+x, pac.getY()+y);
+			}
+		}
+	}
+	
+	private void followThePath(Iterable<Integer> iterable){
+		 LinkedQueue<Integer> arr = ( LinkedQueue<Integer>)iterable;
+		 ArrayList<Integer> arr2 = new ArrayList<Integer>();
+		 for(Integer i:arr){
+			 arr2.add(i);
+		 }
+		 int prev_step = pac.getX()+pac.getY()*labirinth.X();
+		for(int k = arr2.size()-1; k >= 0; k--){
+			int last_x = pac.getX();
+			int last_y = pac.getY();
+			int i = arr2.get(k);
+			//DOWN
+			if(prev_step-i==10){
+				pac.move(pac.getX(), pac.getY()-1);
+				//add(new GLine(last_x*BLOCK_SIZE +BLOCK_SIZE/2, last_y*BLOCK_SIZE +BLOCK_SIZE/2,last_x*BLOCK_SIZE +BLOCK_SIZE/2, pac.getY()*BLOCK_SIZE +BLOCK_SIZE/2));
+			}
+			//TOP
+			if(prev_step-i==-10){
+				pac.move(pac.getX(), pac.getY()+1);
+				//add(new GLine(last_x*BLOCK_SIZE +BLOCK_SIZE/2, last_y*BLOCK_SIZE +BLOCK_SIZE/2,last_x*BLOCK_SIZE +BLOCK_SIZE/2, pac.getY()*BLOCK_SIZE +BLOCK_SIZE/2));
+			}
+			//LEFT
+			if(prev_step-i==1){
+				pac.move(pac.getX()-1, pac.getY());
+				//add(new GLine(last_x*BLOCK_SIZE +BLOCK_SIZE/2, last_y*BLOCK_SIZE +BLOCK_SIZE/2,pac.getX()*BLOCK_SIZE +BLOCK_SIZE/2, last_y*BLOCK_SIZE +BLOCK_SIZE/2));
+			}
+			//RIGHT
+			if(prev_step-i==-1){
+				pac.move(pac.getX()+1, pac.getY());
+				//add(new GLine(last_x*BLOCK_SIZE +BLOCK_SIZE/2, last_y*BLOCK_SIZE +BLOCK_SIZE/2,pac.getX()*BLOCK_SIZE +BLOCK_SIZE/2, last_y*BLOCK_SIZE +BLOCK_SIZE/2));
+			}
+			prev_step = i;
+			//System.out.println("GOTO " + i);
+		}
+		this.remove(diamond);
+	}
+	
+>>>>>>> Stashed changes
 }
